7008cadc45d4aaee7a34da293e587dffd532b1b6
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-10 17:35:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ class Person {
 		thhis.name=name;
 		this.age=age;
 	}
-}
-
-class Student extends Person {
-	Constructor(){
-		super(name);
-	}
 	get name(){
 		return this.name
 	}
 	set age(age){
 		 this.age=age;
 	}
+}
+
+class Student extends Person {
+	Constructor(){
+		super(name);
+	}
+	
 	
 	study(){
-		console.log(`${name} is studying`);
+		console.log(`${this.name} is studying`);
 	}
 }
 
@@ -27,7 +28,7 @@ class Teacher extends Person {
 		super(name);
 	}
 	teach(){
-		console.log(`${name} is studying`);
+		console.log(`${this.name} is teaching`);
 	}
 }
 
